589fe608f3732e06d0d57e91169c7087c098288c
Henning Gebhard <henning.gebhard@rub.de>
2021-03-26 08:23:06 +0100

    fix header template
    remove unused publication addresses, place export date in revisionDesc,
    fix whitespace

--- fiduswriter/document/static/js/modules/exporter/tei/templates/header.js
+++ fiduswriter/document/static/js/modules/exporter/tei/templates/header.js
@@ -1,40 +1,31 @@
 export const header = (authors, title, date, keywords, subtitle) => `
 <teiHeader>
     <fileDesc>
-    <titleStmt>
-        <title type="full">
-        ${title}
-        ${subtitle}
-        </title>
-        ${authors}
-    </titleStmt>
-    <editionStmt>
-        <edition>
-        ${date}
-        </edition>
-    </editionStmt>
-    <publicationStmt>
-        <publisher>Test: Fidus Writer</publisher>
-        <address>
-        <addrLine>...</addrLine>
-        <addrLine>...</addrLine>
-        <addrLine>...</addrLine>
-        </address>
-    </publicationStmt>
-    <sourceDesc>
-        <p>Based on a Fidus Writer document</p>
-    </sourceDesc>
+        <titleStmt>
+            ${title}
+            ${subtitle}
+            ${authors}
+        </titleStmt>
+        <publicationStmt>
+            <publisher>Test: Fidus Writer</publisher>
+        </publicationStmt>
+        <sourceDesc>
+            <p>Based on a Fidus Writer document</p>
+        </sourceDesc>
     </fileDesc>
+    <revisionDesc>
+        <change when=${date}>exported from Fidus Writer</change>
+    </revisionDesc>
     <encodingDesc>
-    <appInfo>
-        <application ident="Fidus-Writer-TEI-Exporter" version="0.1">
-        <label>Fidus Writer TEI Exporter</label>
-        </application>
-    </appInfo>
+        <appInfo>
+            <application ident="Fidus-Writer-TEI-Exporter" version="0.1">
+            <label>Fidus Writer TEI Exporter</label>
+            </application>
+        </appInfo>
     </encodingDesc>
     <profileDesc>
-    <textClass>
-        ${keywords}
-    </textClass>
+        <textClass>
+            ${keywords}
+        </textClass>
     </profileDesc>
 </teiHeader>`
